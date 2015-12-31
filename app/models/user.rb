dcd836aaa6f7bc64ed9a6b5b6053cf73e8f572c2
ianng89 <me@ian.ng>
2015-12-31 14:31:42 +0800

    login done

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+	attr_accessor :password
+	before_save :encrypted_password
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates_format_of :email, :with => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, message: "EMAIL NOT VALID", multiline: true
+	has_many :questions
+	has_many :answers
+
+
+    # This is where the real work is done, store the BCrypt has in the
+    # database
+	    def encrypted_password
+	      # if password.present?
+	      self.encrypted_password = BCrypt::Password.create(password)
+		  end    
+	    
+  	
+
+	# def self.authenticate(email, password)
+	# 	if user = find_by_email(email)
+	# 		if BCrypt::Password.new(user.encrypted_password).is_password? password
+	# 			return user
+	# 		end
+	# 	end
+	# 	return 'No such user / Invalid password'
+	# end
+end
+
