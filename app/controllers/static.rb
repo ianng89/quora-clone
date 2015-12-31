de76687ccf1eca22b7c2f603e61bfbb86d7eff52
ianng89 <me@ian.ng>
2015-12-31 14:31:21 +0800

    login done

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,20 @@
+# require '../models/user'
+require 'byebug'
 get '/' do
+
   erb :"static/index"
 end
+
+post '/signup' do
+  user = User.new(params[:user])
+  byebug
+  if user.save
+    # what should happen if the user is save?
+    @msg = "Welcome to Quora"
+    redirect '/'
+  else
+    # what should happen if the user keyed in invalid date?
+    @msg = "Invalid signup, please try again."
+    redirect '/'
+  end
+end  
